235539a924327da9dd5162b6aaf8f6066ddf3525
Tom Bacci <tbacci.dev@gmail.com>
2018-01-31 18:58:54 +0100

    Feat login page

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index()
+    {
+        // replace this line with your own code!
+        return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, AuthenticationUtils $authUtils)
+    {
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+}
